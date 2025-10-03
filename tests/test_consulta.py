b48184d3f470b4cf73cd64b54a2d5bf0e2a5b98b
jose anderson <jose.anderson10@hotmail.com>
2025-10-03 16:17:06 -0300

    Correção do testes automatizados

--- tests/test_consulta.py
+++ tests/test_consulta.py
@@ -12,14 +12,18 @@ def test_cadastrar_consulta():
         "INSERT INTO pacientes (nome, cpf, data_nasc, sexo) VALUES (%s, %s, %s, %s)",
         ("Paciente Consulta", "88888888888", "1995-05-05", "F")
     )
+
     paciente = db.fetchone("SELECT id FROM pacientes WHERE cpf = %s", ("88888888888",))
+    assert paciente is not None, "Paciente não foi inserido corretamente"
+
+    paciente_id = list(paciente.values())[0]  # garante que pega o valor do id
 
     # Inserir consulta
-    consulta_service.db.execute(
+    db.execute(
         "INSERT INTO consultas (id_paciente, data, especialidade) VALUES (%s, %s, %s)",
-        (paciente["id"], "2025-10-10", "Cardiologia")
+        (paciente_id, "2025-10-10", "Cardiologia")
     )
 
-    consulta = db.fetchone("SELECT * FROM consultas WHERE id_paciente = %s", (paciente["id"],))
-    assert consulta is not None
+    consulta = db.fetchone("SELECT * FROM consultas WHERE id_paciente = %s", (paciente_id,))
+    assert consulta is not None, "Consulta não foi inserida corretamente"
     assert consulta["especialidade"] == "Cardiologia"
--- tests/test_procedimento.py
+++ tests/test_procedimento.py
@@ -12,14 +12,18 @@ def test_cadastrar_procedimento():
         "INSERT INTO pacientes (nome, cpf, data_nasc, sexo) VALUES (%s, %s, %s, %s)",
         ("Paciente Procedimento", "77777777777", "1990-03-03", "M")
     )
+
     paciente = db.fetchone("SELECT id FROM pacientes WHERE cpf = %s", ("77777777777",))
+    assert paciente is not None, "Paciente não foi inserido corretamente"
+
+    paciente_id = list(paciente.values())[0]  # garante que pega o valor do id
 
     # Inserir procedimento
-    procedimento_service.db.execute(
+    db.execute(
         "INSERT INTO procedimentos (id_paciente, data, procedimento) VALUES (%s, %s, %s)",
-        (paciente["id"], "2025-11-11", "Exame de Sangue")
+        (paciente_id, "2025-11-11", "Exame de Sangue")
     )
 
-    procedimento = db.fetchone("SELECT * FROM procedimentos WHERE id_paciente = %s", (paciente["id"],))
-    assert procedimento is not None
+    procedimento = db.fetchone("SELECT * FROM procedimentos WHERE id_paciente = %s", (paciente_id,))
+    assert procedimento is not None, "Procedimento não foi inserido corretamente"
     assert procedimento["procedimento"] == "Exame de Sangue"
